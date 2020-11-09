ac4726fd1b1a7f276c2325af6a167286e9470752
Stinger101 <john.ngugi716@strathmore.edu>
2020-11-09 08:20:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tutorialloginapplication/data/LoginDataSource.java
@@ -0,0 +1,33 @@
+package com.example.tutorialloginapplication.data;
+
+import com.example.tutorialloginapplication.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            if (username=="John" && password == "password") {
+                LoggedInUser fakeUser =
+                        new LoggedInUser(
+                                java.util.UUID.randomUUID().toString(),
+                                "Jane Doe");
+                return new Result.Success<>(fakeUser);
+            }else{
+                return new Result.Error(new Exception("Error logging in"));
+            }
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
